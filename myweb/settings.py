8fdba4f1d40307b1ae616ab5b5cac60a84993171
Taha <ghaderi68akram@gmail.com>
2026-09-03 01:53:42 +0330

    Prepare Django for deployment

--- myweb/settings.py
+++ myweb/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+w*$e6)gqmv!3+@1^r!ah7!34$nnxxy=zwiu7wdhkdo9c6h2jb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
